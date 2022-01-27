6eee9f90d6dc38fb33026ea11eb15a0b4ccc1a0e
Admin <juanvelasquez23141@elpoli.edu.co>
2022-01-27 09:48:51 -0500

    correcion de ruta

--- src/services/constants.tsx
+++ src/services/constants.tsx
@@ -1,5 +1,5 @@
 const production = true;
-export const backendBaseUrl = production ? 'https://curriculo-apit-backend.herokuapp.com' : 'http://127.0.0.1:3000/';
+export const backendBaseUrl = production ? 'https://curriculo-apit-backend.herokuapp.com/' : 'http://127.0.0.1:3000/';
 
 export const getHeaders = ()=>{
   let token = localStorage.getItem('token');
