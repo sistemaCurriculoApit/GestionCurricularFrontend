e624488cd37f26b949d7d86361c86b4daa4c3bda
admin <sistemaCurriculoApit@gmail.com>
2022-07-07 21:16:46 -0500

    cambio url produccion

--- src/services/constants.tsx
+++ src/services/constants.tsx
@@ -1,4 +1,4 @@
-const production = false;
+const production = true;
 export const backendBaseUrl = production ? 'https://curriculo-apit-backend.herokuapp.com/' : 'http://127.0.0.1:3000/';
 export const frontendBaseUrl = production ? 'https://curriculo-apit-frontend.herokuapp.com/' : 'http://127.0.0.1:3002/';
 
